c303f2461d7d4f6fdf5d46170dcb7ece8b0245d7
MarzioCostantini <costantinimarzio@gmail.com>
2023-02-16 15:56:27 +0100

    Add Meta zu App,js

--- src/App.js
+++ src/App.js
@@ -9,19 +9,29 @@ import Overview from "./Pages/Overview";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="wrapper">
-        <Routes>
-          <Route path="/" element={<Overview />}></Route>
-          <Route path="/grid-generator" element={<GridGenerator />}></Route>
-          {/* <Route path="/shadow-generator" element={<ShadowGenerator />}></Route>
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title> PowerRator - Your all in one CSS Generator</title>
+        <meta
+          name="description"
+          content="Your all in one PowerRator tool when it comes to CSS. Whether a complex CSS grid layout, a shadow or beautiful gradients. Try it now, FREE! "
+        />
+      </Helmet>
+      <BrowserRouter>
+        <div className="wrapper">
+          <Routes>
+            <Route path="/" element={<Overview />}></Route>
+            <Route path="/grid-generator" element={<GridGenerator />}></Route>
+            {/* <Route path="/shadow-generator" element={<ShadowGenerator />}></Route>
           <Route
             path="/gradient-generator"
             element={<LinearGradientGenerator />}
           ></Route> */}
-        </Routes>
-      </div>
-    </BrowserRouter>
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
